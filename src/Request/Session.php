6eb79c03183482fcb83c29006dbf3cdb458b7359
Emanuele Corradini <korra88@gmail.com>
2016-03-15 17:41:45 +0100

    Added a method in Session class to open php's session, using alexa's provided id.

--- src/Request/Session.php
+++ src/Request/Session.php
@@ -12,8 +12,33 @@ class Session {
 
 	public function __construct($data) {
 		$this->user = new User($data['user']);
-		$this->sessionId = isset($data['sessionId']) ? $data['sessionId'] : null;
+		$this->sessionId = isset($data['sessionId']) ? $this->parseSessionId($data['sessionId']) : null;
                 $this->new = isset($data['new']) ? $data['new'] : null;
 	}
+        
+        /**
+         * Remove "SessionId." prefix from the send session id, as it's invalid
+         * as a session id (at least for default session, on file).
+         * @param type $sessionId
+         * @return type
+         */
+        protected function parseSessionId($sessionId) {
+                $prefix = 'SessionId.';
+                if (substr($sessionId, 0, strlen($prefix)) == $prefix) {
+                        return substr($sessionId, strlen($prefix));
+                } else {
+                        return $sessionId;
+                }
+        }
+        
+        /**
+         * Open PHP SESSION using amazon provided sessionId, for storing data about the session.
+         * Session cookie won't be sent.
+         */
+        public function openSession() {
+                ini_set('session.use_cookies', 0); # disable session cookies
+                session_id($this->sessionId);
+                return session_start();
+        }
 
 }
