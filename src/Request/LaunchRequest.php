101e537d0f7805570f520bafbea6a27c63ef95be
Jakub Suchy <jakub@prometheus>
2016-03-24 13:44:54 -0400

    There should not be a reason to call the constructor twice

--- src/Request/LaunchRequest.php
+++ src/Request/LaunchRequest.php
@@ -6,7 +6,6 @@ class LaunchRequest extends Request {
 	public $applicationId;
 
 	public function __construct($data) {
-		parent::__construct($data);
 
 		$this->applicationId = $data['session']['application']['applicationId'];
 	}
